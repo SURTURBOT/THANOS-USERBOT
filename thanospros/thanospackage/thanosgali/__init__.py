1b793c5832c1ae9d415434019973aed78894ddfd
RISHABH ANANAD <94896142+thanosuser@users.noreply.github.com>
2022-05-21 08:16:45 -0700

    Add files via upload

---
+++ thanospros/thanospackage/thanosgali/__init__.py
@@ -0,0 +1,2 @@
+from thanospros import *
+from thanospros.thanospackage.thanoshelp.vids import *
---
+++ thanospros/thanospackage/thanosgali/gali.py
@@ -0,0 +1,181 @@
+from thanospros import bot as THANOSBOT
+from thanospros.cmdhelp import CmdHelp
+from thanospros.utils import admin_cmd
+
+x = True
+
+
+@bot.on(admin_cmd(pattern="abuse"))
+async def yashraid(event):
+    while x != False:
+
+        await event.delete()
+        kek = event.chat_id
+        await THANOSBOT.send_message(kek, "TERI MA KI CHOOXT MAARUU BSDKEE")
+        await THANOSBOT.send_message(kek, "TERI BHN KA BHOSXDA FAAD DIA MADARCHOD")
+        await THANOSBOT.send_message(kek, "TERI BHN KI GANXD ME DYNAMTE LAGA KE UDA DIA")
+        await THANOSBOT.send_message(kek, "LEGEND OP HAI HM BAAP BOLTE HMKO")
+        await THANOSBOT.send_message(kek, "TERI MAA KO CHOODUU")
+        await THANOSBOT.send_message(kek, "MAADARCHODD")
+        await THANOSBOT.send_message(kek, "😂")
+        await THANOSBOT.send_message(kek, "ROJJ MERE")
+        await THANOSBOT.send_message(kek, "LODEE PE")
+        await THANOSBOT.send_message(kek, "AAKEE UCHALTII")
+        await THANOSBOT.send_message(kek, "HAII")
+        await THANOSBOT.send_message(kek, "TERI GANND TOD DI JAYEGI MADDRCHOOD")
+        await THANOSBOT.send_message(kek, "😂")
+        await THANOSBOT.send_message(kek, "TERI MAA KA BHOOSSDA FAADU DOGLE MADRCHODD")
+        await THANOSBOT.send_message(kek, "BEHEN")
+        await THANOSBOT.send_message(kek, "KII CHUTTT")
+        await THANOSBOT.send_message(kek, "AUR GANND")
+        await THANOSBOT.send_message(kek, "MAR MARKE")
+        await THANOSBOT.send_message(kek, "EK KAR DIYAA")
+        await THANOSBOT.send_message(kek, "THANOS MEMBER IS ALWAYS ON FIRE")
+        await THANOSBOT.send_message(kek, "😂")
+        await THANOSBOT.send_message(kek, "MADERRRRCHODDO")
+        await THANOSBOT.send_message(kek, "TUM SABB KAA")
+        await THANOSBOT.send_message(kek, "MADERRRRCHODDO")
+        await THANOSBOT.send_message(kek, "TUM SABB KAA")
+        await THANOSBOT.send_message(kek, " BAAAP HAII")
+        await THANOSBOT.send_message(
+            kek, "THANOS HAI HM HMKO KAHTE H OP AUR BETE TUM THANOS KE LAND KI TOPI"
+        )
+        await THANOSBOT.send_message(kek, "😂")
+        await THANOSBOT.send_message(kek, "TERI GAMD TOD DI JAYEGI MADDRCHOD")
+        await THANOSBOT.send_message(kek, "KII MAA")
+        await THANOSBOT.send_message(kek, "KAA RAPEE")
+        await THANOSBOT.send_message(kek, "KR DIYAA")
+        await THANOSBOT.send_message(kek, "EKDUM GHODII")
+        await THANOSBOT.send_message(kek, "BANAKEE")
+        await THANOSBOT.send_message(kek, " CHODAA")
+        await THANOSBOT.send_message(kek, "LEGEND NE CHOD DIA TUJE")
+        await THANOSBOT.send_message(kek, "😂")
+        await THANOSBOT.send_message(kek, "TUMHARII MAA KO")
+        await THANOSBOT.send_message(kek, "AISAA")
+        await THANOSBOT.send_message(kek, "CHODAA GHARR")
+        await THANOSBOT.send_message(kek, "TAKK")
+        await THANOSBOT.send_message(kek, "APNII CHUTT")
+        await THANOSBOT.send_message(kek, "PAKADD")
+        await THANOSBOT.send_message(kek, "KEE")
+        await THANOSBOT.send_message(kek, "ROTII")
+        await THANOSBOT.send_message(kek, "GAEE HAII")
+        await THANOSBOT.send_message(kek, "AB BHII")
+        await THANOSBOT.send_message(kek, "TIME HAI")
+        await THANOSBOT.send_message(kek, "HUME BAAP")
+        await THANOSBOT.send_message(kek, "BOLDOO")
+        await THANOSBOT.send_message(kek, "😂")
+        await THANOSBOT.send_message(kek, "AISE BHII")
+        await THANOSBOT.send_message(kek, "TUMHARII MAA")
+        await THANOSBOT.send_message(kek, "TO ROZZ HUMSEE")
+        await THANOSBOT.send_message(kek, "CHUDTI HI HAII")
+        await THANOSBOT.send_message(kek, "ROZZ KALAPTI HAII")
+        await THANOSBOT.send_message(kek, "WO DEKHO")
+        await THANOSBOT.send_message(kek, "TERA BAAP")
+        await THANOSBOT.send_message(kek, "BETHA HAI")
+        await THANOSBOT.send_message(kek, "GHODEE PEE")
+        await THANOSBOT.send_message(
+            kek,
+            "TERI GANND TOD DI JAYEGI MADDRCHOD TUMHARA BAAP HU ME LEGEND WALE GANND FAAD KE RAKH DENGE TERI MADARCHOOD",
+        )
+        await THANOSBOT.send_message(kek, "TUMHARII")
+        await THANOSBOT.send_message(kek, "MAA")
+        await THANOSBOT.send_message(kek, "MEREEE")
+        await THANOSBOT.send_message(kek, "LODEEE")
+        await THANOSBOT.send_message(kek, "OHH YEAHHH")
+        await THANOSBOT.send_message(kek, "KYAA MASTT")
+        await THANOSBOT.send_message(kek, "MAZAA DETI")
+        await THANOSBOT.send_message(kek, "HAII")
+        await THANOSBOT.send_message(kek, "TUMHARII MAA")
+        await THANOSBOT.send_message(kek, "LEGEND NE TUMHARI BHN KO PURII RAT CHOXDAAA ")
+        await THANOSBOT.send_message(kek, "😂")
+        await THANOSBOT.send_message(kek, "TERI MAKI CHOOT 3 BAAR")
+        await THANOSBOT.send_message(kek, "BAAP HAII H HUM")
+        await THANOSBOT.send_message(kek, "TERII MAA")
+        await THANOSBOT.send_message(kek, "KOO")
+        await THANOSBOT.send_message(kek, "BHUTT GANDAA CHODAA")
+        await THANOSBOT.send_message(kek, "BHOXDIKE AUKAAT BNA FRR LEGEND SE PANGE LENA")
+        await THANOSBOT.send_message(kek, "GANDUUU")
+        await THANOSBOT.send_message(kek, "KIII MAAA")
+        await THANOSBOT.send_message(kek, "KIII CHUTTT")
+        await THANOSBOT.send_message(kek, "LETE SAMAY")
+        await THANOSBOT.send_message(kek, "USKII")
+        await THANOSBOT.send_message(kek, "CHUTTT")
+        await THANOSBOT.send_message(kek, "MEEE MAINEE")
+        await THANOSBOT.send_message(kek, "GARAM LOHEE")
+        await THANOSBOT.send_message(kek, "KAA SARIYAA")
+        await THANOSBOT.send_message(kek, "DAAL DIYAA")
+        await THANOSBOT.send_message(kek, "KALAP GYII")
+        await THANOSBOT.send_message(
+            kek, "TERI BHN KE GORE BOOBS AAHAAH AAH AAHA AAAHA AAHA AAAHH"
+        )
+        await THANOSBOT.send_message(kek, "TERI MA KI CHOOXT FAD DI MENE BHOOSSDIKE ")
+        await THANOSBOT.send_message(kek, "TERE BAAP")
+        await THANOSBOT.send_message(kek, "RHEGE HUM")
+        await THANOSBOT.send_message(kek, "HUMESAA")
+        await THANOSBOT.send_message(kek, "ROZZ TMHARIII")
+        await THANOSBOT.send_message(kek, "MAA KOO")
+        await THANOSBOT.send_message(kek, "GHODII BANAKEE")
+        await THANOSBOT.send_message(kek, "CHODEEEGEE")
+        await THANOSBOT.send_message(kek, "TERI GAMD TOD DI JAYEGI MADDRCHOD KIII")
+        await THANOSBOT.send_message(kek, "MAAAA")
+        await THANOSBOT.send_message(kek, "CHUDD GYIII")
+        await THANOSBOT.send_message(kek, "😂")
+        await THANOSBOT.send_message(kek, "TERA ASLIII")
+        await THANOSBOT.send_message(kek, "BAAAP")
+        await THANOSBOT.send_message(kek, "MAIN HI HUINN YASH")
+        await THANOSBOT.send_message(kek, "MERAA NAJAYAZ🤣")
+        await THANOSBOT.send_message(kek, "BETA HAI TUU")
+        await THANOSBOT.send_message(
+            kek, "TERI MAA KI CHUUTTT AHHHH GZB BHSODDA BNA DIYAA"
+        )
+        await THANOSBOT.send_message(
+            kek, "HUMSE BAKCHODI NA KRRR TELE PR RHNE LAYAK NHI BACHEGA"
+        )
+        await THANOSBOT.send_message(kek, "MAAADDRRCHHHOOODD")
+        await THANOSBOT.send_message(kek, "LOOWDEE KEE BAAL")
+        await THANOSBOT.send_message(kek, "LAAWDDDEEE KEE BAAL")
+        await THANOSBOT.send_message(kek, "JHHAAATTT SEE SHAKAL KE")
+        await THANOSBOT.send_message(kek, "CHINARRR MADRCHODDD")
+        await THANOSBOT.send_message(kek, "RAMDI KI AULAAD")
+        await THANOSBOT.send_message(kek, "TERRIII MAAA KO TERE SAMNE CHOODTA HU")
+        await THANOSBOT.send_message(kek, "BSSDDKKEE BAAP SE PANGE LENGE")
+        await THANOSBOT.send_message(kek, "YEEE LODDUUU KE BAAL")
+        await THANOSBOT.send_message(kek, "JHHAAATTT BHAR KI AAUUKAAT NHI H")
+        await THANOSBOT.send_message(
+            kek, "AAYYYY HHHHH BAAAAPPP SSSEEE BHIIIDDHNEE MADARCHHHHOODDD"
+        )
+        await THANOSBOT.send_message(
+            kek, "BBHHOOSSDDDIIKEEEE APNIII MAA KO DWAI LAA KRRRR DEE"
+        )
+        await THANOSBOT.send_message(
+            kek,
+            "MAADDRRRCCCHHHOOODDDD TERIIII MAA KIII CCCHHHUUUTTT SSSEEE KHUUNNN NIKALLLL DIYAA HH BBEETTIICCHHOODD",
+        )
+        await THANOSBOT.send_message(kek, "CHAKKKAAA HHH TUUUU MAAADDRRCHHHOODDDD")
+        await THANOSBOT.send_message(kek, "JJHHHAAATEEE JITNI")
+        await THANOSBOT.send_message(kek, "BHIIIII BAAADDIII HOO")
+        await THANOSBOT.send_message(kek, "RHHHTTAAAA LUNNNNNDD KEEE NICHEE HI HHHH")
+        await THANOSBOT.send_message(kek, "BETICCCHHOOODD")
+        await THANOSBOT.send_message(
+            kek, "BHHHHOOOSSSDDIIIIKKEEEE TEEERRIIII AAUUKKAAAT MERA MUUUTT"
+        )
+        await THANOSBOT.send_message(kek, "PIINEEE KI HHHH USSEEE PIII OR KKKHUUSSHHH")
+        await THANOSBOT.send_message(kek, "RAHHHH BETIIICHHOOODDD CHIINARRR")
+        await THANOSBOT.send_message(kek, "AAJJJ KEEE BAAD LEGEND SE PANGA")
+        await THANOSBOT.send_message(kek, "MATTT LEENNAA MADRCHOODDD")
+        await THANOSBOT.send_message(kek, "NHIII TTOO AGLA")
+        await THANOSBOT.send_message(kek, "NUMBERRR TERE PURI KHANDANNN")
+        await THANOSBOT.send_message(kek, "KIIII ORRTEEEE KAA HOGAAA")
+        await THANOSBOT.send_message(kek, "CHINNARRRR BHOSDIKAAA")
+        await THANOSBOT.send_message(kek, "LEGEND SE H BHNCCHOOODDDD")
+        await THANOSBOT.send_message(kek, "YAHIII PATAKKK KR")
+        await THANOSBOT.send_message(kek, "CHHHOOOODDDD DENGE")
+        await THANOSBOT.send_message(kek, "TUMMM JESEE KITNOOO")
+        await THANOSBOT.send_message(kek, "KOOO CHOOOOOOODDDDD KRRR CHOD DIYAA GANDUUU")
+        await THANOSBOT.send_message(
+            kek,
+            "TELEGRAM ME RAHNA HAI TO THANOS KO PAPA BOLNA PAREGA WARNA TUMHARII BHN XUDEGI AISE HI ROJ",
+        )
+
+
+CmdHelp("gali").add_command("abuse", None, "Use And See To Stop .reload").add()
